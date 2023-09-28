811a0d4bd9310d59de1630328635e3848117f545
Jacek <jacek@opcode.pl>
2023-09-28 22:59:19 +0200

    Remove some types

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,22 @@ import axios from "axios";
 
 function App() {
     const fetchImages = async () => {
-        const response = await axios.get<ImageInterface[]>('https://pixabay.com/api/?key=39676804-5edb90c96ed25e80d3dabf5a6&q=food&orientation=horizontal&per_page=10');
+        const response = await axios.get('https://pixabay.com/api/?key=39676804-5edb90c96ed25e80d3dabf5a6&q=food&orientation=horizontal&per_page=10');
         return response.data;
     }
 
     //should I always type response - data variable? use generics?
-    const {isLoading, error, data} = useQuery<ImageInterface[]>({
+    const {isLoading, error, data} = useQuery({
         queryKey: ['images'],
         queryFn: fetchImages,
+        select: (data) => data.hits
     });
 
     if (isLoading) return 'Loading...'
     if (error) return 'An error has occurred: ' + error
 
     //@todo check if there are any results
-    const images: ImageInterface[] = data.hits as ImageInterface[];
+    const images: ImageInterface[] = data as ImageInterface[];
 
     return <>
         <div className="container row">
