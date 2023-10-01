3eb18d728390f483140322ae2eb73dee5acbb8f8
Jacek Różański <jacek@opcode.pl>
2023-10-02 00:18:11 +0200

    Remove unused snapshot

--- tests/Domain/Image/Image.test.tsx
+++ tests/Domain/Image/Image.test.tsx
@@ -19,26 +19,3 @@ test('App', async () => {
     //expect(screen.getByText(/preview-url/i)).toBeDefined();
     //expect(screen.findByText(/webformat-url/i)).toBeDefined();
 });
-
-
-// describe('App', () => {
-//     it('renders single image', () => {
-//         let sampleImage: ImageInterface = {
-//             id: 123456,
-//             previewURL: "preview=-url",
-//             type: "type-x",
-//             user: "user-a",
-//             webformatURL: "webformat-url"
-//         };
-//
-//         //render(<Image id={sampleImage.id} previewUrl={sampleImage.previewURL} type={sampleImage.type} user={sampleImage.user} webformatURL={sampleImage.webformatURL} />);
-//         render(<Image {...sampleImage} />);
-// //screen.debug();
-// expect(screen.getByText('123456'));
-//         // expect(screen.getByText(/123456/i)).toBeDefined();
-//         // expect(screen.getByText(/preview-url/i)).toBeDefined();
-//         // expect(screen.getByText(/type-x/i)).toBeDefined();
-//         // expect(screen.getByText(/user-a/i)).toBeDefined();
-//         //expect(screen.findByText(/webformat-url/i)).toBeDefined();
-//     });
-// });
